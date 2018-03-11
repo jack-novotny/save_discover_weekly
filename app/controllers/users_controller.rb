4ff845bb2a6d883e087ff01bce96c3b074c9dd16
Jack Novotny <jack-novotny@att.net>
2018-03-11 16:25:03 -0500

    Add routing to make a logged in homepage, test pulling data from user's spotify account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def spotify
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    # respond_to do |format|
+    #   format.html { redirect_to root_url(spotify_user) }
+    # end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/auth/spotify/callback', to: 'users#spotify'
+  get '/users/spotify', to: 'homes#index'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get 'homes/index/:spotify_user', to: 'homes#index'
   root 'homes#index'
   get '*path', to: redirect('/')
 end
