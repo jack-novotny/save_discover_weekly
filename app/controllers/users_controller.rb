f5a232fdc7b26350fae5e9987985be41a1e400d0
Jack Novotny <jack-novotny@att.net>
2018-03-11 17:11:05 -0500

    Find user's discover weekly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @playlists = find_playlists
+    find_current_discover_weekly
+  end
+
+  private
+  def find_playlists
+    @spotify_user.playlists(limit: 50)
+  end
+
+  def find_current_discover_weekly
+    @playlists.each do |playlist|
+      if playlist.name == "Discover Weekly"
+        @current_discover_weekly = playlist
+      end
+    end
   end
 end
